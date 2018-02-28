eb182ecfaff4ae5e414687c559603ac377ae930a
Jacob <jacob.logan2002@gmail.com>
2018-02-27 21:36:37 -0500

    .

--- main.py
+++ main.py
@@ -8,39 +8,10 @@ Created on Wed Jan 31 20:24:38 2018
 from Profiles.Fighter import *
 from Profiles.Thief import *
 from Profiles.Mage import *
+from Profiles.Create import *
 #Charater creator
 print('Welome to Final Fantasy.py')
-print('lets create a character')
 
-
-print('Next, What class do you want to be?')
-print('each class has its own stregnth and weaknesses so choose wisely')
-
-print(' F - Fighter: a brash, stotic hero ready to fight at a moments notice')
-print(' T - Thief: A sneaky but clever hero, with a tool for every situation')
-print(' M - Mage: A wise and elegant hero that use magic to fight evil')
-
-classChoice = input()
-createChoice = True
-while createChoice:
-    if classChoice.upper() == 'F':
-        F = Fighter()   
-        attributes = F.create()
-    if classChoice.upper() == 'T':
-        T = Thief()
-        attributes = T.create()
-    if classChoice.upper() == 'M':
-        M = Mage()
-        attributes = M.create()        
-    print('strength = ' + str(attributes[0]))
-    print('dexerity = ' + str(attributes[1]))
-    print('wisdom = ' + str(attributes[2]))
-    print('intelligence = ' + str(attributes[3]))
-    print('constitution = ' + str(attributes[4]))
-    print('charisma = ' + str(attributes[5]))
-    createChoice = input('do you want to reroll Y/N? ')
-if createChoice.upper() == 'n':
-    createChoice = False
 
     
         
